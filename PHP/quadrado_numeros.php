e0eec5502ce985eef1761013636801a4c8da510d
gillianfranco <gillian.melo.franco17@gmail.com>
2024-01-23 18:04:44 -0300

    Pasta PHP e arquivo quadrado_numeros.php

---
+++ PHP/quadrado_numeros.php
@@ -0,0 +1,10 @@
+<?php
+    // Sequência
+    $sequencia = 5;
+
+    while($sequencia != 0){
+
+        echo "{$sequencia} * {$sequencia} = " . pow($sequencia, 2) . PHP_EOL;
+
+        $sequencia--;
+    }
