f419d7bccc235b129447a64a1170b8150fb0b37f
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-13 15:39:43 -0300

    potenciacao.cpp

---
+++ C e C++/4-potenciacao.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void escreva(string texto){
+    cout << texto;
+}
+
+void calculaPotencia(int base, int expoente){
+    int i = 1;
+    int res = 1;
+
+    if(expoente == 0){
+        escreva("\nO resultado e' 1.\n");
+    }else{
+        while(i <= expoente){
+            res *= base;
+            i++;
+        }
+        escreva("\nO resultado e' ");
+        cout << res << ".\n";
+    }
+}
+
+int main(){
+    int base, expoente, continuar;
+
+    do{
+        escreva("Digite a base da potencia: ");
+        cin >> base;
+        escreva("\nDigite o valor do expoente: ");
+        cin >> expoente;
+        calculaPotencia(base, expoente);
+
+
+        escreva("Deseja calcular outra potencia? (Digite 1 para SIM | Digite 0 para NAO)");
+        cin >> continuar;   
+    }while(continuar == 1);
+    
+    return 0;
+}
