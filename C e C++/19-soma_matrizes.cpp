e98728e0a8c67cb6adbbf983d02b507695c4d2bd
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-26 22:29:10 -0300

    Modificar arrays

--- C e C++/19-soma_matrizes.cpp
+++ C e C++/19-soma_matrizes.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main(){
-    int dimension[2][2];
+    int r[2], c[2];
     
     for(int i = 0 ; i < 2 ; i++){
         cout << "Type the number of rows in the array [" << i + 1 << "]: ";
-        cin >> dimension[i][i];
+        cin >> r[i];
         cout << "Type the number of columns in the array [" << i + 1 << "]: ";
-        cin >> dimension[i][i+1];
+        cin >> c[i];
     }
 
-    int *array1 = new int[dimension[0][0]][dimension[0][1]];
-    int *array2 = new int[dimension[1][0]][dimension[1][1]];
+    int *array1 = new int[r[0]][c[0]];
+    int *array2 = new int[r[1]][c[1]];
 
     cout << endl << "--- Array 1 ---" << endl;
-    for(int i = 0; i < dimension[0][0]; i++){
-        for(int j = 0; j < dimension[0][1]; j++){
+    for(int i = 0; i < r[0]; i++){
+        for(int j = 0; j < c[0]; j++){
             cout << "Type a number for position [" << i << "][" << j << "] for the array [1]: ";
             cin >> array1[i][j];
-        }
+        } 
     }
 
     cout << endl << "--- Array 2 ---" << endl;
