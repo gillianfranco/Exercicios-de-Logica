9435be5f127b29f72841728aae8f6866275baee8
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-26 22:55:09 -0300

    Imprimir resultados

--- C e C++/19-soma_matrizes.cpp
+++ C e C++/19-soma_matrizes.cpp
@@ -3,35 +3,42 @@ using namespace std;
 
 int main(){
     int r, c;
+
+    r = 0;
+    c = 0;
     
     cout << "Type the number of rows in the arrays: ";
     cin >> r;
     cout << "Type the number of columns in the arrays: ";
     cin >> c;
 
-    int *array1 = new int[r][c];
-    int *array2 = new int[r][c];
+    int firstArray[r][c];
+    int secondArray[r][c];
 
     cout << endl << "--- Array 1 ---" << endl;
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             cout << "Type a number for position [" << i << "][" << j << "] for the array [1]: ";
-            cin >> array1[i][j];
+            cin >> firstArray[i][j];
         } 
     }
+    cout << endl;
 
     cout << endl << "--- Array 2 ---" << endl;
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             cout << "Type a number for position [" << i << "][" << j << "] for the array [2]: ";
-            cin >> array2[i][j];
+            cin >> secondArray[i][j];
         }
     }
+    cout << endl;
 
-    int *arraySum = new int[r][c];
+    int arraySum[r][c];
+
     for(int i = 0; i < r; i++){
-        for(int j = 0; i < c; i++){
-            arraySum[i][j] = array1[i][j] + array2[i][j];
+        for(int j = 0; j < c; i++){
+            arraySum[i][j] = firstArray[i][j] + secondArray[i][j];
+            cout << firstArray[i][j] << " + " << secondArray[i][j] << " = " << arraySum[i][j] << endl;
         }
     }
  
