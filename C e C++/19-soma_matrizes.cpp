ab67a9bca3870529e35640c329b4e13177d9f8d1
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-26 22:33:57 -0300

    Corrigir erros de lógica

--- C e C++/19-soma_matrizes.cpp
+++ C e C++/19-soma_matrizes.cpp
@@ -2,29 +2,27 @@
 using namespace std;
 
 int main(){
-    int r[2], c[2];
+    int r, c;
     
-    for(int i = 0 ; i < 2 ; i++){
-        cout << "Type the number of rows in the array [" << i + 1 << "]: ";
-        cin >> r[i];
-        cout << "Type the number of columns in the array [" << i + 1 << "]: ";
-        cin >> c[i];
-    }
+    cout << "Type the number of rows in the arrays: ";
+    cin >> r;
+    cout << "Type the number of columns in the arrays: ";
+    cin >> c;
 
-    int *array1 = new int[r[0]][c[0]];
-    int *array2 = new int[r[1]][c[1]];
+    int *array1 = new int[r][c];
+    int *array2 = new int[r][c];
 
     cout << endl << "--- Array 1 ---" << endl;
-    for(int i = 0; i < r[0]; i++){
-        for(int j = 0; j < c[0]; j++){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
             cout << "Type a number for position [" << i << "][" << j << "] for the array [1]: ";
             cin >> array1[i][j];
         } 
     }
 
     cout << endl << "--- Array 2 ---" << endl;
-    for(int i = 0; i < dimension[1][0]; i++){
-        for(int j = 0; j < dimension[1][1]; j++){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
             cout << "Type a number for position [" << i << "][" << j << "] for the array [2]: ";
             cin >> array2[i][j];
         }
