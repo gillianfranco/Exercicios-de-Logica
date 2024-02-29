b5a4897ee768289d4eeb6e3cbf93da7bb12a1da6
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-28 22:11:46 -0300

    Create impressao_de_vetor.cpp

---
+++ C e C++/20-impressao_de_vetor.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size, verify;
+
+    do{
+        cout << "Type the size for the vector: ";
+        cin >> size;
+        cout << endl;
+
+        int *vector = new int[size];
+
+        cout << "--- Insertion of Values ---" << endl;
+        for(int i = 0; i < size; i++){
+            cout << "Type a value for position [" << i << "]: ";
+            cin >> vector[i];
+        }
+
+        cout << "\n--- Values Inserted ---" << endl;
+        for(int i = 0; i << size; i++){
+            cout << "Position [" << i << "] -> Value [" << vector[i] << "]";
+        }
+        do{
+            cout << "\nType 0 to finish the programe or type 1 to continue: ";
+            cin >> verify;
+        }while(verify != 0 && verify != 1);
+        if(verify == 0){
+            delete [] vector;
+        }
+    }while(verify != 0);
+
+    return 0;
+}
