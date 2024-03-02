10f1742147e3f89e203622a5b80375b082712534
gillianfranco <gillian.melo.franco17@gmail.com>
2024-03-01 23:54:24 -0300

    Adicionar quebra de linha

--- C e C++/20-impressao_de_vetor.cpp
+++ C e C++/20-impressao_de_vetor.cpp
@@ -19,12 +19,13 @@ int main(){
 
         cout << "\n--- Values Inserted ---" << endl;
         for(int i = 0; i < size; i++){
-            cout << "Position [" << i << "] -> Value [" << vector[i] << "]";
+            cout << "Position [" << i << "] -> Value [" << vector[i] << "]" << endl;
         }
 
         do{
             cout << "\nType 0 to finish the programe or type 1 to continue: ";
             cin >> verify;
+            cout << endl;
         }while(verify != 0 && verify != 1);
         if(verify == 0){
             delete [] vector;
