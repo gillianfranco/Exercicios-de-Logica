7a32f0df568a354a25ca8a023a84f73a43c73219
gillianfranco <gillian.melo.franco17@gmail.com>
2024-03-06 21:51:13 -0300

    25-juros_compostos.cpp

---
+++ C e C++/25-juros_compostos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float M, C, i;
+    int t;
+
+    cout << "Digite o montante inicial: ";
+    cin >> M;
+
+    cout << "Digite o periodo de tempo (em anos): ";
+    cin >> t;
+
+    cout << "Digite capital investido no periodo: ";
+    cin >> C;
+
+    cout << "Digite a porcentagem (em decimal) no periodo: ";
+    cin >> i;
+
+    cout << i;
+
+    return 0;
+}
