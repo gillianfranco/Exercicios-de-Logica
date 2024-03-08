5103bb2e840429b95e86594cb67f3dbc4ec2acca
gillianfranco <gillian.melo.franco17@gmail.com>
2024-03-07 22:02:02 -0300

    Terminar de fazer um programa simples que calcula a rentabilidade de um investimento durante um período

--- C e C++/25-juros_compostos.cpp
+++ C e C++/25-juros_compostos.cpp
@@ -1,23 +1,82 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(){
-    float M, C, i;
+class rentabilidade{
+    private: 
+    float mInicial, M, C, i;
     int t;
 
-    cout << "Digite o montante inicial: ";
-    cin >> M;
+    public:
+    void setMontanteInicial(float M=0){
+        this->M = M;
+        mInicial = this->M;
+    }
+    void setCapitalInvestido(float C=0){
+        this->C = C;
+    }
+    void setPorcentagemRentabilizada(float i=0){
+        this->i = i;
+    }
+    void setPeriodoDeTempo(int t=0){
+        this->t = t;
+    }
 
-    cout << "Digite o periodo de tempo (em anos): ";
-    cin >> t;
+    float getMontanteInicial(){
+        return mInicial;
+    }
+    float getCapitalInvestido(){
+        return C;
+    }
+    float getPorcentagemRentabilizada(){
+        return i;
+    }
+    int getPeriodoDeTempo(){
+        return t;
+    }
+    float getEvolucaoDoPatrimonio(){
+        this->M += C * pow(1 + i, t);
+        return M;
+    }
+};
+
+int main(){
+    float *M = new float;
+    float *C = new float;
+    float *i = new float;
+    int *t = new int;
+
+    rentabilidade VALE3;
+
+    cout << "Digite o montante inicial: ";
+    cin >> *M;
+    VALE3.setMontanteInicial(*M);
+
+    cout << "Digite o periodo de tempo (em meses): ";
+    cin >> *t;
+    VALE3.setPeriodoDeTempo(*t);
 
     cout << "Digite capital investido no periodo: ";
-    cin >> C;
+    cin >> *C;
+    VALE3.setCapitalInvestido(*C);
 
     cout << "Digite a porcentagem (em decimal) no periodo: ";
-    cin >> i;
+    cin >> *i;
+    VALE3.setPorcentagemRentabilizada(*i);
 
-    cout << i;
+    cout << "\n\n --- Acao da Vale -> " << VALE3.getEvolucaoDoPatrimonio() << " --- \n";
+    cout << "Montante Inicial: " << VALE3.getMontanteInicial() << " reais\n";
+    cout << "Tempo: " << VALE3.getPeriodoDeTempo() << " meses\n";
+    cout << "Capital Investido: " << VALE3.getCapitalInvestido() << " reais\n";
+    cout << "Lucro em porcentagem: " << VALE3.getPorcentagemRentabilizada() << endl;
+
+    delete M;
+    delete C;
+    delete i;
+    delete t;
+    cout << M << endl;
+
+    system("pause >> NULL");
 
     return 0;
 }
